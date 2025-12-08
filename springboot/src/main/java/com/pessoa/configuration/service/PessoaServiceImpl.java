58e5ef7a0fb01eac327fcf0bbfcd6f6e386a236b
daniel6rufino <danieldesouzarufino@gmail.com>
2025-12-07 21:46:05 -0300

    end of development phase
    Signed-off-by: daniel6rufino <danieldesouzarufino@gmail.com>

--- springboot/src/main/java/com/pessoa/configuration/service/PessoaServiceImpl.java
+++ springboot/src/main/java/com/pessoa/configuration/service/PessoaServiceImpl.java
@@ -1,49 +1,82 @@
 package com.pessoa.configuration.service;
 
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import com.pessoa.entity.Pessoa;
-import com.pessoa.repository.PessoaRepository;
+import com.pessoa.repository.PessoaRepositoryPostgres;
 import com.pessoa.service.PessoaService;
 
 @Service
 public class PessoaServiceImpl implements PessoaService {
 
-    private final PessoaRepository repository;
+    private final PessoaRepositoryPostgres repo;
 
-    public PessoaServiceImpl(PessoaRepository repository) {
-        this.repository = repository;
+    private static final Logger log = LoggerFactory.getLogger(PessoaServiceImpl.class);
+
+    public PessoaServiceImpl(PessoaRepositoryPostgres repo) {
+        this.repo = repo;
     }
 
     @Override
     public Pessoa criar(Pessoa pessoa) {
-        return repository.save(pessoa);
+        Pessoa pessoa_criada = repo.save(pessoa);
+        log.info("Pessoa criada com sucesso! ID: {}", pessoa_criada.getId());
+        return pessoa_criada;
     }
 
     @Override
     public Page<Pessoa> listar(Pageable pageable) {
-        return repository.findAll(pageable);
+        log.info("Pessoas listadas com sucesso!");
+        return repo.findAll(pageable);
     }
 
     @Override
     public Pessoa pesquisar(Long id) {
-        return repository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
+        log.info("Buscando pessoa com, ID: {}", id);
+
+        return repo.findById(id)
+            .orElseThrow(() ->  new RuntimeException("Pessoa não encontrada, - ID {"+ id +"}"));
     }
 
     @Override
     public Pessoa atualizar(Long id, Pessoa novaPessoa) {
+        log.info("Atualizar pessoa com, ID {}", id);
+
         Pessoa existente = pesquisar(id);
         existente.setNome(novaPessoa.getNome());
         existente.setDtNascimento(novaPessoa.getDtNascimento());
         existente.setAtivo(novaPessoa.isAtivo());
-        return repository.save(existente);
+    
+        Pessoa pessoa_atualizada = repo.save(existente);
+        log.info("Pessoa atualizada com sucesso, ID {}", pessoa_atualizada.getId());
+        return pessoa_atualizada;
     }
 
     @Override
     public void deletar(Long id) {
-        repository.deleteById(id);
+        log.info("Excluindo pessoa com ID: {}", id);
+        repo.deleteById(id);
+        log.info("Pessoa excluída com sucesso! ID: {}", id);
+    }
+
+    @Override
+    public Page<Pessoa> listarTudo(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<Pessoa> resultado = repo.findAll(pageable);
+
+        log.info(
+            "Listagem completa realizada | Página: {} | Registros nesta página: {} | Total de páginas: {} | Total de registros: {}",
+             page,
+            resultado.getNumberOfElements(),
+            resultado.getTotalPages(),
+            resultado.getTotalElements()
+        );
+
+        return resultado;
     }
 }
---
+++ springboot/src/main/java/com/pessoa/repository/PessoaRepositoryPostgres.java
@@ -0,0 +1,7 @@
+package com.pessoa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.pessoa.entity.Pessoa;
+
+public interface PessoaRepositoryPostgres extends JpaRepository<Pessoa, Long> {
+}
